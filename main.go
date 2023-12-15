4bf5f33c0655e4b224a7914c92b13b74b31bbfe2
nmowens95 <nmowens95@gmail.com>
2023-12-15 16:19:34 -0600

    Needed to update methods to all caps in my middleware, also clight format change to html

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "Get, Post, Put, Delete, Options")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
