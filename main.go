660049981e099dd7745e5e8e3294c74a43c56039
nmowens95 <nmowens95@gmail.com>
2023-12-18 21:12:30 -0600

    Moved middleware to a new file

--- main.go
+++ main.go
@@ -21,16 +21,3 @@ func main() {
 	log.Print("Listening...")
 	log.Fatal(srv.ListenAndServe())
 }
-
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
