4ed5a8f7add7d8970cff6fd1fe316b286dad1056
nmowens95 <nmowens95@gmail.com>
2023-12-22 15:16:16 -0600

    Wrote the update task logic. Haven't tested it yet, added in a specific error code due to not being able to post JSON and needed to trouble shoot. Also realized I was missing a question mark, had 3, needed 4, for the values sql statement on the insert

--- handlers.go
+++ handlers.go
@@ -31,10 +31,10 @@ func CreateTask(w http.ResponseWriter, r *http.Request) {
 	defer mu.Unlock()
 
 	// insert task into db
-	insert := "INSERT INTO tasks (Name, Description, Comment, Status) VALUES (?, ?, ?)"
+	insert := "INSERT INTO tasks (Name, Description, Comment, Status) VALUES (?, ?, ?, ?)"
 	_, err := DB.Exec(insert, task.Name, task.Description, task.Comment, task.Status)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Not able to insert task, something went wrong", http.StatusInternalServerError)
 		return
 	}
 
@@ -99,14 +99,43 @@ func GetTasks(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateTask(w http.ResponseWriter, r *http.Request) {
+	taskID := chi.URLParam(r, "id")
+	id, err := strconv.Atoi(taskID)
+	if err != nil {
+		http.Error(w, "Task not found", http.StatusBadRequest)
+		return
+	}
 
+	var updatedTask Task
+	if err := json.NewDecoder(r.Body).Decode(&updatedTask); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	result, err := DB.Exec("UPDATE tasks SET Name = ?, Description = ?, Comment = ?, Status = ? WHERE ID = ?", updatedTask.Name, updatedTask.Description, updatedTask.Comment, updatedTask.Status, id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rowsAff, _ := result.RowsAffected()
+	if rowsAff == 0 {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func DeleteTask(w http.ResponseWriter, r *http.Request) {
 	taskID := chi.URLParam(r, "id")
 	id, err := strconv.Atoi(taskID)
 	if err != nil {
-		http.Error(w, "Task ID not found", http.StatusBadRequest)
+		http.Error(w, "Task ID not found", http.StatusNotFound)
+		return
 	}
 
 	mu.Lock()
