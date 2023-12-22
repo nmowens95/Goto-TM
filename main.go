83dddedd4c20ab9f210fec3b8cd4d840df9f636b
nmowens95 <nmowens95@gmail.com>
2023-12-22 15:18:31 -0600

    Added my router functions, moved the defer DB.Close() toward the bottom. Included and error message, which is a duplication and aI can take out. I tested a Post and Get request with succes. Still need to try put and delete

--- main.go
+++ main.go
@@ -5,18 +5,31 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	openDB()
+	if DB == nil {
+		log.Fatal("DB is not initialized properly")
+	}
+
 	port := "8080"
 	router := chi.NewRouter()
+	router.Use(middleware.Logger)
 	router.Handle("/", http.FileServer(http.Dir(".")))
-	corsRouter := middlewareCors(router)
+	router.Get("/tasks", GetTasks)
+	router.Get("/tasks/{id}", GetTask)
+	router.Post("/tasks", CreateTask)
+	router.Put("/tasks/{id}", UpdateTask)
+	router.Delete("/tasks/{id}", DeleteTask)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
-		Handler: corsRouter,
+		Handler: router,
 	}
+
+	defer DB.Close()
 
 	log.Print("Listening...")
 	log.Fatal(srv.ListenAndServe())
