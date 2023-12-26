ce569a46b59b11940327958b632bd99a8c279a3d
nmowens95 <nmowens95@gmail.com>
2023-12-26 13:02:46 -0600

    Have logic for adding a user to the database.

---
+++ create_user.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	addUser := "INSERT INTO user (Email) VALUES (?)"
+	result, err := DB.Exec(addUser, user.Email)
+	if err != nil {
+		http.Error(w, "There was an issue adding this user", http.StatusInternalServerError)
+		return
+	}
+
+	// Get ID of the las inserted user
+	userID, _ := result.LastInsertId()
+	user.ID = int(userID) // Assign inserted user's Id to the user Struct
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
+}
